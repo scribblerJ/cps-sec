6043c1e7e6ea96cde920d85cd7e6506f949eafcf
(유정규) <(yjg950324@naver.com)>
2020-04-09 23:19:15 +0900

    INDENT ERR FIXED

--- indeed.py
+++ indeed.py
@@ -19,13 +19,12 @@ class Scraper():                                                    # html데이
 
     def getCards(self, soup, cnt):                                       #각 페이지 내의 모든 카드 불러오는 함수
         jobCards = soup.find_all("div", class_ = "jobsearch-SerpJobCard")   #job~클래스를 갖는 div태그 중 요소들의 모든 html정보
-
         jobTitle = []
         jobID = []
         jobLocation = []
 
         for j in jobCards:
-            jobID.append("https://kr.indeed.com/viewjob?jk=" + j["data-jk"])#j["data-jk"] = ?
+            jobID.append("http://kr.indeed.com/viewjob?jk=" + j["data-jk"])#j["data-jk"] = ?
             jobTitle.append(j.find("a").text.replace("\n",""))
                                                                             # append = ()안에 매개변수를 배열 마지막에 추가
                                                                             # j = jobCards 내의 전체 요소
@@ -37,29 +36,30 @@ class Scraper():                                                    # html데이
             elif j.find("span",class_="location") !=None:
                 jobLocation.append(j.find("span",class_="location").text)
                                                                             # print(len(jobID), len(jobTitle), len(jobLocation)) = 각 배열 별 값의 갯수가 잃치하는 지 점검
-            self.writeCSV(jobID, jobTitle, jobLocation, cnt)
+        self.writeCSV(jobID, jobTitle, jobLocation, cnt)
 
     def writeCSV(self, ID, Title, Location, cnt):                   #csv 파일 생성함수
-        file = open("indeed.csv", "a",encoding="UTF-8", newline="")                          #open(파일명[,mode[,buffering]]) = 파일을 열거나 새로 생성
+        file = open("indeed.csv", "a",encoding="UTF-8", newline="")          #open(파일명, a, endcoding, newline) => a = 파일 마지막에 새로운 내용 추가, encoding, newline = ?
         wr = csv.writer(file)
         for i in range(len(ID)):
-            wr.writerow([str(i + 1 + cnt * 50), ID[i], Title[i], Location[i]])
-        file.close
+            wr.writerow([str((i + 1) + (cnt * 50)), ID[i], Title[i], Location[i]])
+            print(i + 1, cnt * 50)
+        file.close()
 
 
     def scrap(self):
-        file = open("indeed.csv", "w", encoding="UTF-8", newline="")
+        file = open("indeed.csv", "w",encoding="UTF-8", newline="")         #open(파일명, w, encoding, newline) => w = 파일 열고 새로 쓰기
         wr = csv.writer(file)
-        wr.writerow(["No. ", "Link", "Tiltle", "Location"])
-        file.close
+        wr.writerow(["No.", "Link", "Title", "Location"])
+        file.close()
 
-        soupPage = self.getHTML(0)
-        pages = self.getPages(soupPage)        
+        soupPage = self.getHTML(0)                                          #cnt = 0 즉 첫페이지부터 불러와서
+        pages = self.getPages(soupPage)                                     #getPagas 함수로 전체 페이지 수 구하기
 
-        for i in range(pages):
-            soupCard = self.getHTML(i)
-            self.getCards(soupCard, i)
-            print(i +1 , "번쨰 페이지 Done")
+        for i in range(pages):                                              #전체 페이지 수 만큼 반복
+            soupCard = self.getHTML(i)                                      #해당 페이지의 html정보 불러오기
+            self.getCards(soupCard, i)                                      #해당 페이지의 html정보내에서 모든 카드 불러오기 (cnt = 해당 페이지 번호)
+            print(i+1 , "번쨰 페이지 Done")
 
 if __name__ == "__main__":
     s = Scraper()
